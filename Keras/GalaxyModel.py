4abe8e859c77b29dce459408b8a690c4bd14fc56
Bruno <bruno.afonso.alves@ist.utl.pt>
2018-06-29 13:46:04 +0200

    macro working although the input pipeline should be improved

--- Keras/GalaxyModel.py
+++ Keras/GalaxyModel.py
@@ -5,10 +5,14 @@ from tensorflow.python.keras.datasets import mnist
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Dense, Dropout, Flatten
 from tensorflow.python.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.python.keras.utils import to_categorical
 from tensorflow.python.keras import backend
+from tensorflow.python.keras.losses import categorical_crossentropy
+from tensorflow.python.keras.optimizers import Adadelta
 
 import os
 import glob
+import gc
 import numpy as np
 from PIL import Image
 
@@ -39,9 +43,9 @@ def picture_decoder(tf_session, picture_name, height, width):
     return tf_session.run( final_tensor, feed_dict={picture_name_tensor: picture_name} )
 
 
-def get_data_dict(DataFolder, Classes, Extension='jpg'):
+def get_data(DataFolder, Classes, Height=300, Width=300, Extension='jpg'):
     """
-    Creates a dictionary with 3D data tuples as keys and numerical labels as values
+    Creates two arrays: one with pictures and the other with numerical labels
     Each class must have a separate subfolder, and all classes must lie inside the same data folder.
 
     Arguments:
@@ -50,7 +54,7 @@ def get_data_dict(DataFolder, Classes, Extension='jpg'):
     3. Extension (string): extension of the pictures inside DataFolder. Only 'jpg' is supported. 
 
     Returns:
-    1. A 4D-tuple with pictures and another tuple with labels
+    1. A 4d array with pictures and another array with labels. The first array follows the following format: (index, height_in_pixels, width_in_pixels, numer_of_channels)
     2. The given classes are converted into numerical labels, starting from zero. For example, if three classes are present, 'a', 'b' and 'c', then the labels with respectively be 0, 1 and 2.
     """
 
@@ -69,82 +73,113 @@ def get_data_dict(DataFolder, Classes, Extension='jpg'):
 
     ###Tensorflow Picture Decoding###
     data_tuple = ()
-    input_height, input_width, input_depth = 300, 300, 3
+    input_height, input_width, input_depth = Height, Width, 3
     image_pattern_list = []
     temp_number = 0
 
-    pict_array_length = 0
-    for nameClass in Classes:
-        pict_array_length += len (glob.glob( os.path.join(DataFolder,nameClass,"*."+Extension) )[:10] )
-    pict_array = np.zeros((pict_array_length, input_width, input_width, input_depth), dtype=np.float32)
-    lable_array = np.zeros(pict_array_length, dtype=np.uint8)
+    pict_array_length = np.zeros(len(Classes), dtype=np.uint32)
+    for iClass,nameClass in enumerate(Classes):
+        pict_array_length[iClass]=len(glob.glob(os.path.join(DataFolder,nameClass,"*."+Extension))[:50])
+ 
+    pict_array_length_total = sum(pict_array_length)
+    pict_array = np.zeros((pict_array_length_total, input_width, input_width, input_depth), 
+                          dtype=np.float32)
+    label_array = np.zeros(pict_array_length_total, dtype=np.uint8)
 
     with tf.Session() as sess:
         init = tf.group( tf.global_variables_initializer(), tf.local_variables_initializer() )
         sess.run(init)
 
-        for nameClass in Classes:
-            for i,pict in enumerate(glob.glob(os.path.join(DataFolder,nameClass,"*."+Extension))[:10]):
+        for iClass, nameClass in enumerate(Classes):
+            for i,pict in enumerate(glob.glob(os.path.join(DataFolder,nameClass,"*."+Extension))[:50]):
+                index = i + iClass*pict_array_length[iClass]
+                if index%20 == 0:
+                    print(index+1, "pictures have been decoded", 
+                          float(index)/float(pict_array_length_total)*100, "%")
                 #ndarray with dtype=float32
-                pict_array[i] = picture_decoder(sess, pict, input_height, input_width) 
-            
-    print(pict_array.shape)
-    return data_tuple
+                pict_array[index] = picture_decoder(sess, pict, input_height, input_width) 
+                label_array[index] = iClass
+    
+    print("Shape of the array of pictures:", pict_array.shape)
+    print("Shape of the array of labels:", label_array.shape)
+    return (pict_array, label_array)
 
 
-mypath = "/data1/alves/galaxy_photos_balanced_gap/"
-myclasses = ('before', 'during')
-img_rows, img_cols = 2048, 2048
-num_classes, epochs, batch_size = 2, 2, 128
-print( get_data_dict(mypath, myclasses, 'jpg') )
+def split_data(x, y, fraction=0.8):
+    """
+    Splits the data into 'training' and 'testing' datasets according to the specified fraction.
+
+    Arguments:
+    1. The actual data values ('x')
+    2. The label array ('y')
+    3. The fraction of training data the user wants
+
+    Returns:
+    The testing and training data and labels in the following order:
+    (x_train, y_train, x_test, y_test)
+    """
+    ###Sanity Check###
+    if len(x) != len(y):
+        print("ERROR: The arrays of the values and of the labels must have the same size!")
+        quit()
+    
+    splitting_value = int(len(x)*fraction)
+    return x[:splitting_value], y[:splitting_value], x[splitting_value:], y[splitting_value:]
 
 
-#(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-"""
-if backend.image_data_format() == 'channels_first':
-    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
-    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
-    input_shape = (1, img_rows, img_cols)
-else:
-    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-    input_shape = (img_rows, img_cols, 1)
+def main(_):
+    myclasses = ('before', 'during')
+    mypath = "/data1/alves/galaxy_photos_balanced_gap/"
+    img_rows, img_cols = 300, 300
+    num_classes, epochs, batch_size = 2, 10, 64
+    x, y = get_data(mypath, myclasses, img_rows, img_cols, 'jpg')
 
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
-print('x_train shape:', x_train.shape)
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
+    if backend.image_data_format() == 'channels_first':
+        x = x.reshape(x.shape[0], 3, img_rows, img_cols)
+        input_shape = (3, img_rows, img_cols)
+    else:
+        x = x.reshape(x.shape[0], img_rows, img_cols, 3)
+        input_shape = (img_rows, img_cols, 3)
+        
+    x_train, y_train, x_test, y_test = split_data(x, y, fraction=0.8)
+    x_train = x_train.astype('float32')
+    x_test = x_test.astype('float32')
+    x_train /= 255
+    x_test /= 255
 
-# convert class vectors to binary class matrices (one-hot encoding)
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
+    print('x_train shape:', x_train.shape, "; y_train shape", y_train.shape)
+    print(x_train.shape[0], 'train samples')
+    print(x_test.shape[0], 'test samples')
 
-model = Sequential()
-model.add(Conv2D(32, kernel_size=(3, 3),
-                 activation='relu',
-                 input_shape=input_shape))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-model.add(Flatten())
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(num_classes, activation='softmax'))
+    # convert class vectors to binary class matrices (one-hot encoding)
+    y_train = to_categorical(y_train, num_classes)
+    y_test = to_categorical(y_test, num_classes)
 
-model.compile(loss=keras.losses.categorical_crossentropy,
-              optimizer=keras.optimizers.Adadelta(),
-              metrics=['accuracy'])
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(5, 5), activation='relu',
+                     data_format=backend.image_data_format(),
+                     input_shape=input_shape))
+    model.add(Conv2D(64, (5, 5), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(num_classes, activation='softmax'))
 
-model.fit(x_train, y_train,
-          batch_size=batch_size,
-          epochs=epochs,
-          verbose=1,
-          validation_data=(x_test, y_test))
-score = model.evaluate(x_test, y_test, verbose=0)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
-"""
+    model.compile(loss=categorical_crossentropy,
+                  optimizer=Adadelta(),
+                  metrics=['accuracy'])
+
+    model.fit(x_train, y_train,
+              batch_size=batch_size,
+              epochs=epochs,
+              verbose=1,
+              validation_data=(x_test, y_test))
+    score = model.evaluate(x_test, y_test, verbose=0)
+    print('Test loss:', score[0])
+    print('Test accuracy:', score[1])
+
+if __name__ == '__main__':
+    tf.app.run(main=main)
